de7535e366385300bd84975da3d90580b4eb1cd6
Anton-twista <extw1ster@yandex.ru>
2023-03-02 14:59:57 +0300

    upload

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+
+$('body').append('<div class="upbtn"></div>');            
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.upbtn').css({
+            bottom: '50px'
+        });
+        } else {
+        $('.upbtn').css({
+            bottom: '-60px'
+        });
+    }
+});
+$('.upbtn').on('click',function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
